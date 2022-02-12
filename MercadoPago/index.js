c403b997f59645eedd710e45b73e35dcb845fa6b
Claudisnei Alves <claudisneibello@gmail.com>
2022-02-12 13:38:19 -0300

    Gerando os pagamentos - Criado rota para carregar o pagamento do produto

--- MercadoPago/index.js
+++ MercadoPago/index.js
@@ -6,15 +6,57 @@ const app = express();
 
 //setar a configuração do mercado pago
 mercadoPago.configure({
-    sendBox: true,
+    sandBox: true,
     access_token: "TEST-3968865630091953-021112-55b106e9d39971879c1930b459f86c35-82586186"
 });
 
 //criando as rotas da aplicação
 //rota principal da aplicação
 app.get('/',(req, res)=>{
-    res.send("Olá mundo");
+    res.send("Olá mundo"+Date.now());
 })
+
+//rota para pagamentos
+app.get('/pagar', async (req, res)=>{
+
+    //variavel id do produto
+    var id = "" + Date.now();
+    
+    //email do pagador
+    var emailPagador = "c_b_alves@hotmail.com";
+    
+    //dados do pagamento
+    var dados = {
+        items: [
+            item = {
+                //pode ser uuid , pode ser a data que a venda foi gerada com dia,mes,ano,hora,minuto,milisegunod
+                //aqui vamos utilizar a data
+                id: id,
+                title: "2x video games; 3 camisas",
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(150)
+            }
+        ],
+        payer:{
+            email: emailPagador
+        },
+        external_reference: id
+    }
+
+    try {
+        //criando a variável de pagamentos
+    var pagamento = await mercadoPago.preferences.create(dados);
+    console.log(pagamento);
+
+    //url de pagamentos
+    return res.redirect(pagamento.body.init_point);
+        
+    } catch (err) {
+        return res.send(err.message);
+    }
+    
+});
 
 
 
